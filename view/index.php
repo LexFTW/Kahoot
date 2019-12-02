21e8f2a060b8905ef5f1679eb7ade1ad0117b47f
LexFTW <lexmengual@gmail.com>
2019-12-02 17:44:02 +0100

    02122019_1743

--- view/index.php
+++ view/index.php
@@ -7,45 +7,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" href="../public/css/bootstrap.min.css">
-	<!-- <link rel="stylesheet" href="../public/css/fontawesome_all.css">
-	<script src="../public/css/fontawesome_all.js" charset="utf-8"></script> -->
+	<title>Kahoot</title>
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" href="../public/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../public/css/stylesheet.css">
 </head>
 <body>
 	<main class="container-fluid">
 		<div class="row justify-content-center">
 			<div class="col-md-6">
-				<div class="card">
-					<div class="card-body">
-						<form class="" action="index.html" method="post">
-							<!-- Inicio Contraseña -->
-							<div class="form-group">
-								<div class="input-group mb-3">
-								  <div class="input-group-prepend">
-								    <span class="input-group-text" id="user-addon">
-											<i class="fas fa-users"></i>
-										</span>
-								  </div>
-								  <input type="text" class="form-control" placeholder="Nombre de Usuario" aria-label="Nombre de Usuario" aria-describedby="user-addon">
+				<div id="login">
+					<div class="card login__card">
+						<div class="card-header login__card-header">
+							<img class="img-fluid mx-auto d-block" src="../public/img/a3531f69e64b18edd7599b446bb4e025f988a856.svg" alt="Logo Kahoot" title="Logo Kahoot" />
+						</div>
+						<div class="card-body">
+							<form class="" action="index.html" method="post">
+								<!-- Inicio Contraseña -->
+								<div class="form-group">
+									<div class="input-group mb-3 col-9 mx-auto">
+									  <div class="input-group-prepend login__input-group-prepend">
+									    <span class="input-group-text" id="user-addon">
+												<i class="fas fa-users"></i>
+											</span>
+									  </div>
+									  <input type="text" class="form-control" placeholder="Nombre de Usuario" aria-label="Nombre de Usuario" aria-describedby="user-addon">
+									</div>
 								</div>
-							</div>
-							<!-- Fin Contraseña -->
-							<!-- Inicio Contraseña -->
-							<div class="form-group">
-								<div class="input-group mb-3">
-								  <div class="input-group-prepend">
-								    <span class="input-group-text" id="password-addon">
-											<i class="fas fa-users"></i>
-										</span>
-								  </div>
-								  <input type="text" class="form-control" placeholder="Contraseña" aria-label="Contraseña" aria-describedby="password-addon">
+								<!-- Fin Contraseña -->
+								<!-- Inicio Contraseña -->
+								<div class="form-group">
+									<div class="input-group mb-3 col-9 mx-auto">
+									  <div class="input-group-prepend">
+									    <span class="input-group-text" id="password-addon">
+												<i class="fas fa-users"></i>
+											</span>
+									  </div>
+									  <input type="text" class="form-control" placeholder="Contraseña" aria-label="Contraseña" aria-describedby="password-addon">
+									</div>
 								</div>
-							</div>
-							<!-- Fin Contraseña -->
-							<input type="submit" class="btn btn-primary" name="submit" value="Iniciar Sesión">
-						</form>
+								<!-- Fin Contraseña -->
+								<input type="submit" class="btn btn-primary mx-auto d-block login__button-submit" name="submit" value="Iniciar Sesión">
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
