928e2be78f7a3727d2ce2ded557125fc54f108e0
rayaalmudena <raya.almudena@gmail.com>
2019-12-03 16:06:40 +0100

    v

--- controller/pinController.php
+++ controller/pinController.php
@@ -4,13 +4,13 @@
 		$stm = $pdo->prepare("SELECT id FROM `room` where pin=".$_POST['pin']." and status='playing';");
 		$stm->execute();
 		$row = $stm->fetchAll();
-				
+
 		if($row){
-			$_SESSION['room']=$row[0];			
-			echo"<div class='alert alert-info' role='alert'>In</div>";
+			$_SESSION['room']=$row[0][0];			
+			echo"<div class='col-8 mx-auto alert alert-info' role='alert'>You are in!</div>";
 		}else {
 			//echo '<script type="text/javascript">','window.location.href ="../php/namePin.php";','</script>';
-	   		echo"<div class='alert alert-danger' role='alert'>This is a danger alert—check it out!</div>";
+	   		echo"<div class='col-8 mx-auto alert alert-danger' role='alert'>Wrong pin!</div>";
 		}
 	}
   ?>
