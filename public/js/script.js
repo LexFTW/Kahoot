8a60e8916a4feddec84fb38238da3797ea68ed57
LexFTW <lexmengual@gmail.com>
2019-12-03 17:57:18 +0100

    03122019_1757

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+<tr>
+  <td class="align-middle">Kahoot 2</td>
+  <td class="align-middle">
+    <button type="button" class="btn ml-2 btn-primary float-right" name="button">
+      <i class="fas fa-edit"></i>
+    </button>
+    <button type="button" class="btn btn-success float-right" name="button">
+      <i class="fas fa-play"></i>
+    </button>
+  </td>
+</tr>
