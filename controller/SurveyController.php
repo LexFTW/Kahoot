c0117ed923298e4b463d76415d4eb948ade7fd8e
Roger <rogertim3@gmail.com>
2019-12-08 20:17:46 +0100

    creacion answer and surweys2

--- controller/SurveyController.php
+++ controller/SurveyController.php
@@ -6,3 +6,10 @@
     $query = "SELECT * FROM survey";
     return $db->select($query, ['id', 'name']);
   }
+
+  function createSurvey(){
+  	$db = Database::getInstance("localhost","kahoot","root","");
+	$isInsert = $db -> insert($table,$data);
+	return $isInsert;
+  }
+ ?>
--- controller/answerController.php
+++ controller/answerController.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
+	
 	include "../model/Database.php";
 
-	// createQuestion('question',['surveyID' => '1', 'text' => "pregunta2"]);
-
-	function createQuestion($table,$data){
+	function createAnswer($table,$data){
 		$db = Database::getInstance("localhost","kahoot","root","");
 		$isInsert = $db -> insert($table,$data);
 		return $isInsert;
 	}
+
  ?>
