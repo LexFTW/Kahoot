2b7526968dca68a53ccecb9c10e7bf6e828910db
LexFTW <lexmengual@gmail.com>
2019-12-15 22:11:36 +0100

    klñdfjañlfksd

--- routes/LobbyRoute.php
+++ routes/LobbyRoute.php
@@ -8,19 +8,20 @@ session_start();
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
   $pin = new PinCodeController;
   $_SESSION['pin'] = $pin->generateCodePin();
+  print_r($_POST);
 }
 
-echo $_SESSION['pin'];
+// echo $_SESSION['pin'];
 
-if(empty($_SESSION['pin'])){
-  header('Location: index.php');
-}
-
-$url = $_SERVER['REQUEST_URI'];
-header("Refresh: 15; URL = $url");
-
-$room = new RoomController;
-$players = $room->getPlayers($_SESSION['pin']);
+// if(empty($_SESSION['pin'])){
+//   header('Location: index.php');
+// }
+//
+// $url = $_SERVER['REQUEST_URI'];
+// header("Refresh: 15; URL = $url");
+//
+// $room = new RoomController;
+// $players = $room->getPlayers($_SESSION['pin']);
 
 
 
