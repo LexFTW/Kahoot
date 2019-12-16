79aecae352e547a2e40c1d80014b1d6191403acc
Roger <rogertim3@gmail.com>
2019-12-17 00:18:15 +0100

    jaa pablo picaso

--- controller/SurveyController.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "../model/Database.php"; 
-
-  function getSurvey(){
-    $db = Database::getInstance("localhost","kahoot","root","");
-    $query = "SELECT * FROM survey";
-    return $db->select($query, ['id', 'name']);
-  }
- ?>
